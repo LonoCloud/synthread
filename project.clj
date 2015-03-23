aa0ba7268b10835a953fd69d65e5f77283c0e5f0
Hoàng Minh Thắng <p@banphim.net>
2015-03-23 12:49:46 +0700

    only support clojure v1.6.0

--- project.clj
+++ project.clj
@@ -6,12 +6,4 @@
             :distribution :repo
             :comments "same as Clojure"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}}
-  :aliases {"1.2" ["with-profile" "1.2"]
-            "1.3" ["with-profile" "1.3"]
-            "1.4" ["with-profile" "1.4"]
-            "1.5" ["with-profile" "1.5"]})
+  :dependencies [[org.clojure/clojure "1.6.0"]])
