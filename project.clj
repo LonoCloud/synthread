47883577933c21751a91916433de9287900446e1
Hoàng Minh Thắng <p@banphim.net>
2015-03-24 16:25:05 +0700

    config cljx and cljs in project.clj

--- project.clj
+++ project.clj
@@ -5,5 +5,33 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
+  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-3126"]]
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :profiles {:dev {:plugins [[com.keminglabs/cljx "0.6.0"]
+                             [lein-cljsbuild "1.0.5"]
+                             [lein-figwheel "0.2.5"]
+                             [lein-pdo "0.1.1"]]
+                   :cljx {:builds [{:source-paths ["src"]
+                                    :output-path "src"
+                                    :rules :clj}
+
+                                   {:source-paths ["src"]
+                                    :output-path "src"
+                                    :rules :cljs}
+                                   {:source-paths ["test"]
+                                    :output-path "test"
+                                    :rules :clj}
+
+                                   {:source-paths ["test"]
+                                    :output-path "test"
+                                    :rules :cljs}]}
+                   :cljsbuild {:builds {:test
+                                        {:source-paths ["src" "test"]
+                                         :notify-command ["nodejs" "target/simple.js"]
+                                         :compiler {:main test-runner
+                                                    :output-to "target/simple.js"
+                                                    :output-dir "target/out"
+                                                    :target :nodejs
+                                                    :optimizations :none}}}}}})
